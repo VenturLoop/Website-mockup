94fc153d3dbdaab63808cb85cb01d71f63ef4009
Atharva <teteatharva@gmail.com>
2025-06-14 14:35:07 +0530

    Navigation Bar change

--- src/lib/Note.js
+++ src/lib/Note.js
@@ -1,5 +1,8 @@
 // API call in 
 
-// 1.landing page - Venturloop count founder, incestor, Total match & startup pitched
+// 1.landing page - Venturloop count founder, incestor, Total match & startup pitched , 
+//                  Investor image data 
+//                  Email Send in Subscribe
+//                  
 
 // 2.
